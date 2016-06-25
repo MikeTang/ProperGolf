8f3f477d0c882149977bde1f1a8315906a3bd770
Mike Tang <mtminded@gmail.com>
2016-06-25 15:24:43 -0700

    First Commit

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Laravel</title>
+        <link href='https://fonts.googleapis.com/css?family=Raleway:400,200,500,700' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
+    </head>
+    <body>
+        
+        <header class="container-fluid">
+        
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 logo">
+                        <div>Logo</div>
+                    </div>
+                    <div class="col-md-8 nav">
+                        <ul>
+                            <li>Log In</li>
+                            <li>Sign Up</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+        </header>
+        
+        
+        @yield('content')
+
+    </body>
+</html>
