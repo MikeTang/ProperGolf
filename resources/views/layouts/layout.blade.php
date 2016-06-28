7577f6b6734ffc31a3ea5856311cad1cbc1b90ad
Mike Tang <mtminded@gmail.com>
2016-06-27 22:22:14 -0700

    Merged google fonts with the main css

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Laravel</title>
-        <link href='https://fonts.googleapis.com/css?family=Raleway:400,200,500,700' rel='stylesheet' type='text/css'>
+        {{-- <link href='https://fonts.googleapis.com/css?family=Raleway:400,200,500,700' rel='stylesheet' type='text/css'> --}}
         <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
     </head>
     <body>
